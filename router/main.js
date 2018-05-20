1a415e3655da1bd913f587c07317c4677029f492
geonhwa <ghjeong12@postech.ac.kr>
2018-05-20 13:46:39 +0900

    Updated maze

--- router/main.js
+++ router/main.js
@@ -12,6 +12,13 @@ module.exports = function(app, fs)
           res.end(data);
         });
     });
+  app.get('/imgs/dog', function(req, res){
+      fs.readFile('./assets/img/dog.png', function (error, data) {
+          res.writeHead(200, { 'Content-Type':'text/html' });
+          res.end(data);
+        });
+    });
+
   app.get('/imgs/animal-track', function(req, res){
       fs.readFile('./assets/img/animal-track.png', function (error, data) {
           res.writeHead(200, { 'Content-Type':'text/html' });
@@ -30,6 +37,13 @@ module.exports = function(app, fs)
           res.end(data);
         });
     });
+  app.get('/imgs/dog_food', function(req, res){
+      fs.readFile('./assets/img/dog_food.png', function (error, data) {
+          res.writeHead(200, { 'Content-Type':'text/html' });
+          res.end(data);
+        });
+    });
+
   app.get('/imgs/waterfall', function(req, res){
       fs.readFile('./assets/img/waterfall.png', function (error, data) {
           res.writeHead(200, { 'Content-Type':'text/html' });
@@ -42,9 +56,14 @@ module.exports = function(app, fs)
 
   app.get('/',function(req,res){
     res.render('index', {
-    result: "",
-    num_lines: 0
+      formula : "",
+      result: "",
+      num_lines: 0,
+      total_ins: "",
+      calcul_result:"",
+      maze_level : req.body.level
     })
+
   });
   var multer = require('multer');
   var upload = multer({dest: 'uploads/'});
@@ -54,54 +73,85 @@ module.exports = function(app, fs)
 
   /* After uploading an image */
   app.post('/upload', upload.single('image'), function(req, res, next) {
-
+    //console.log(req.body.level);
      // Choose what the Vision API should detect
      // Choices are: faces, landmarks, labels, logos, properties, safeSearch, texts
   var types = ['labels'];
-  client.textDetection(req.file.path)
-  .then(results => {
+  client.textDetection(req.file.path).then(
+    results => {
     const detections = results[0].textAnnotations;
-    //console.log('Text:');
-    //detections.forEach(text => console.log(text));
-    })
-  .catch(err => {
-    console.error('ERROR:', err);
-    });
+    }).catch(
+      err => {
+      console.error('ERROR:', err);
+      }
+    );
 
-  /* sleep function */
-  function sleepFor( sleepDuration ){
-        var now = new Date().getTime();
-            while(new Date().getTime() < now + sleepDuration){ /* do nothing */ }
-  }
-  //var move = require('move-js');
   /* Get documentTextDetection result */
- client.documentTextDetection(req.file.path)
-  .then(results => {
-    const fullTextAnnotation = results[0].fullTextAnnotation;
-    console.log('Document:');
-    console.log(fullTextAnnotation.text);
-    var result = fullTextAnnotation.text;
-    // The last line is dummy line
-    var result_lines = result.split("\n");
-
-    res.render('index',
+  client.documentTextDetection(req.file.path).then(
+    results => {
+      const fullTextAnnotation = results[0].fullTextAnnotation;
+      var result = fullTextAnnotation.text;
+      // The last line is dummy line
+      if(result[result.length-1]=='\n')
+        result = result.slice(0,result.length-1);
+      var happycalculator = require('happycalculator');
+      var formula = '20 * ( 10 + 20 ) / 20';
+      var result_lines = result.split("\n");
+      var total_ins=[];
+      var calc_result;
+      if(result_lines.length == 1)
       {
-        result : fullTextAnnotation.text,
-        num_lines : result_lines.length
-      });
-
+        formula = fullTextAnnotation.text;
+        // There is no : in blocks
+        formula = formula.replace(/:/g,"\/");
+        console.log("[Calculation mode] " + formula);
+        switch(result[0])
+        {
+        case '0' :
+        case '1' :
+        case '2' :
+        case '3' :
+        case '4' :
+        case '5' :
+        case '6' :
+        case '7' :
+        case '8' :
+        case '9' :
+        case '10' :
+        case '\(' :
+        case '-' :
+          calc_result = happycalculator.calculate(formula);
+          break;
+        default:
+          formula="";
+          break;
+      }
+    }
     if(result_lines.length >= 2)
     {
-      console.log("#1 "+ result_lines[0]);
-      console.log("#2 "+ result_lines[1]);
-      //sleepFor(1000);
-      //console.log("one sec");
-      //move('#example-1 .box')
-      //.set('margin-left', 200)
-      //.end();
-      //sleepFor(1000);
-      console.log("two sec");
+      calc_result="";
+      formula="";
+      var ins;
+      for(var i = 0; i < result_lines.length ; i ++)
+      {
+        ins = result_lines[i].split(" ");
+        total_ins.push(ins);
+      }
+      console.log("# of lines : "+ result_lines.length);
+      for(var i = 0; i < result_lines.length;i++)
+      {
+        console.log("#"+i+" "+ result_lines[i]);
+      }
     }
+    res.render('index',
+      {
+        formula : formula,
+        result : fullTextAnnotation.text,
+        num_lines : result_lines.length,
+        total_ins : total_ins,
+        calcul_result : calc_result,
+        maze_level : req.body.level
+      });
 
 
   })
